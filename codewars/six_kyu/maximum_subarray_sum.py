0ed673acfcfe47f5d197f534a74c34fb129d5e8d
DavFilsDev <miharisoadavidfils@gmail.com>
2026-01-03 14:47:08 +0300

    add: Implement maximum subarray sum using Kadane's algorithm (5kyu)

---
+++ codewars/six_kyu/maximum_subarray_sum.py
@@ -0,0 +1,23 @@
+# Kadane’s Algorithm -Solution
+def max_sequence(arr):
+    max_sum = 0
+    current_sum = 0
+
+    for n in arr:
+        current_sum = max(0, current_sum + n)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+if __name__ == "__main__":
+    tests = [
+        [],
+        [-1, -2, -3],
+        [1, 2, 3],
+        [-2, 1, -3, 4, -1, 2, 1, -5, 4],
+        [4, -1, 2, 1],
+        [-5, 4, -1, 2, 1, -5]
+    ]
+
+    for t in tests:
+        print(t, "=>", max_sequence(t))
