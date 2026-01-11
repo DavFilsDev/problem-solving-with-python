9b27c0a03f6d9ce99bcf019480cca88bde977742
DavFilsDev <miharisoadavidfils@gmail.com>
2026-01-11 22:02:09 +0300

    Add lucky ticket number validation

---
+++ codewars/five_kyu/lucky_ticket_check.py
@@ -0,0 +1,28 @@
+def luck_check(st):
+    if not st:
+        raise ValueError("Empty string")
+
+    if not st.isdigit():
+        raise ValueError("Input must be a decimal number")
+
+    n = len(st)
+    half = n // 2
+
+    left = st[:half]
+    right = st[-half:]
+
+    sum_left = sum(int(d) for d in left)
+    sum_right = sum(int(d) for d in right)
+
+    return sum_left == sum_right
+
+tests = [
+    "003111",
+    "813372",
+    "17935",
+    "56328116",
+    "123456"
+]
+
+for t in tests:
+    print(t, "=>", luck_check(t))
